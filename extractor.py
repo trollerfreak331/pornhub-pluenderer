35c8a41dacc3270662660e888c1b4b0b7beae32e
Julius Noack <noack@solutiondrive.de>
2017-11-10 16:22:54 +0100

    made extractor regex private

--- extractor.py
+++ extractor.py
@@ -2,9 +2,9 @@ import re
 
 class Extractor(object):
     def __init__(self):
-        self.url_re = re.compile(r"a.+?href=\".+?viewkey=(.+?)\"", re.MULTILINE)
+        self._url_re = re.compile(r"a.+?href=\".+?viewkey=(.+?)\"", re.MULTILINE)
 
     def extract_viewkeys(self, data):
-        urls = re.findall(self.url_re, data)
+        urls = re.findall(self._url_re, data)
 
         return urls

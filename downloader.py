93e5cff22ac59c482e374221cea755ed70c5274a
j <j@localhost.localdomain>
2017-11-10 17:42:13 +0100

    Added file download

--- downloader.py
+++ downloader.py
@@ -1,6 +1,18 @@
 import requests as http
+from storage import Storage
 
 class Downloader(object):
+    def __init__(self):
+        self._storage = Storage()
+
     def get(self, url):
         rsp = http.get(url)
         return rsp.text
+
+    def save_file(self, url, filename):
+        rsp = http.get(url, stream=True)
+
+        with self._storage.new_file(filename) as file:
+            for chunk in rsp.iter_content(chunk_size=4096):
+                if chunk:
+                    file.write(chunk)

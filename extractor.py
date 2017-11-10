e0cad9f8c82ee4de03dca990244cc1cb410b4c44
Julius Noack <noack@solutiondrive.de>
2017-11-10 16:02:39 +0100

    added key filter

--- extractor.py
+++ extractor.py
@@ -2,13 +2,9 @@ import re
 
 class Extractor(object):
     def __init__(self):
-        self.url_re = re.compile(r"a.+? href=\"(.+?)\"", re.MULTILINE)
+        self.url_re = re.compile(r"a.+?href=\".+?viewkey=(.+?)\"", re.MULTILINE)
 
-    def extract_urls(self, base_url, data):
+    def extract_viewkeys(self, data):
         urls = re.findall(self.url_re, data)
-
-        for i, url in enumerate(urls):
-            if not url.startswith(base_url):
-                urls[i] = base_url + url
 
         return urls

753db18ec6daddd5f0153d5fc17a5a4b91fe142d
Julius Noack <noack@solutiondrive.de>
2017-11-28 10:07:20 +0100

    added clint as standart output

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from downloader import Downloader
 from extractor import Extractor
 
+# https://github.com/kennethreitz/clint cool shit \Q-Q/
+from clint.textui import colored, puts
+
 if __name__ == "__main__":
     dl = Downloader()
     ex = Extractor()
-
     url = "https://pornhub.com"
 
     rsp = dl.get(url)
     viewKeys = ex.extract_viewkeys(rsp)
 
     for key in viewKeys:
-        print(key)
+        puts(colored.green(key))

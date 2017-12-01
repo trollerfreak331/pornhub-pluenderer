8c02df35da49fdd8cc0695af3bd601cb2e9aa794
Julius Noack <noack@solutiondrive.de>
2017-12-01 11:30:31 +0100

    refactored code to match standart python styleguide

--- extractor.py
+++ extractor.py
@@ -1,8 +1,11 @@
 import re
 
+viewkey_re = r"a.+?href=\".+?viewkey=(.+?)\""
+
+
 class Extractor(object):
     def __init__(self):
-        self._url_re = re.compile(r"a.+?href=\".+?viewkey=(.+?)\"", re.MULTILINE)
+        self._url_re = re.compile(viewkey_re, re.MULTILINE)
 
     def extract_viewkeys(self, data):
         urls = re.findall(self._url_re, data)
